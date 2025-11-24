6305f5d4c61157d986885c22bdfe677eeec7f0cd
saif25 <saif25@gmail.com>
2025-11-24 19:19:01 +0530

    feat: add variables.tf file

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "cidr_public_subnet" {}
+variable "ap_availability_zone" {}
+variable "cidr_private_subnet" {}
+
+
+# variable "public_key" {}
+# variable "key_name" {}
+
+variable "ec2_ami_id" {}
